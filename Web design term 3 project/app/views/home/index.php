2bc51b58d6e69b9b0f288e3dc9dee7c1818551af
CezarFlorinel <706431@student.inholland.nl>
2024-02-19 17:07:58 +0100

    removed stuff
    as

--- Web design term 3 project/app/views/home/index.php
+++ Web design term 3 project/app/views/home/index.php
@@ -1,18 +1,27 @@
 <?php
 include __DIR__ . '/../header.php';
+
+
+// Initialize the repository
+$repository = new App\Repositories\userRepository();
+
+// Fetch users
+$users = $repository->getUsers();
 ?>
 
 <h1>Welcome to the MVC Blog!</h1>
 
-<?php
-foreach ($model as $article) {
-?>
+<h2>Our Users</h2>
+<?php if (!empty($users)): ?>
+    <ul>
+        <?php foreach ($users as $user): ?>
+            <li><?= htmlspecialchars($user['Username']) ?> - <?= htmlspecialchars($user['Email']) ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php else: ?>
+    <p>No users found.</p>
+<?php endif; ?>
 
-<h2><?= $article->title ?></h2>
-<p><i><?= $article->posted_at ?></i></p>
-<p><?= $article->content ?></p>
-
 <?php
-}
 include __DIR__ . '/../footer.php';
 ?>
