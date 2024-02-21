fb3bb710c7866222650b995d4ec57402f8a5f109
CezarFlorinel <706431@student.inholland.nl>
2024-02-21 20:43:09 +0100

    Footer
    Footer

--- Web design term 3 project/app/views/footer.php
+++ Web design term 3 project/app/views/footer.php
@@ -1,4 +1,63 @@
-</div>
-</main>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>footer</title>
+    <link rel="stylesheet" href="CSS_files/footer.css">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:500|Zen+Antique|Allerta+Stencil&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <div class="footer-box">
+        <div class="background-footer1">
+
+            <div class="footer-item1">
+                <a href="/"><img src="assets/images/Logos/Logo-W.png" alt="logo" class="footer-logo"></a>
+                <a class="nav-type-1-footer" href="/">Do you have any questions?</a>
+                <a class="nav-type-1-footer" href="/">Contact us</a>
+            </div>
+
+            <div class="footer-item2">
+                <a class="nav-type-1-footer" href="/">Home</a>
+                <a class="nav-type-1-footer" href="/">History</a>
+                <a class="nav-type-1-footer" href="/">Food</a>
+            </div>
+
+            <div class="footer-item3">
+                <a class="nav-type-1-footer" href="/">Theatre</a>
+                <a class="nav-type-1-footer" href="/">Music</a>
+                <a class="nav-type-1-footer" href="/">Museums</a>
+            </div>
+
+            <div class="footer-item4">
+                <a class="nav-type-1-footer" href="/">The Festival</a>
+                <a class="nav-type-1-footer" href="/">A Stroll Through History</a>
+                <a class="nav-type-1-footer" href="/">Dance!</a>
+                <a class="nav-type-1-footer" href="/">Yummy!</a>
+            </div>
+
+            <div class="background-footer2">
+                <div class="footer-item5">
+                    <p class="small-text-footer">&copy;ITZHAARLEM - All rights reserved.</p>
+                    <p class="small-text-footer">Cookies Policy</p>
+                    <p class="small-text-footer">Privacy Policy</p>
+                    <p class="small-text-footer">Terms & Conditions</p>
+                </div>
+
+                <div class="footer-item6">
+                    <a href="/"><img src="assets/images/Logos/Facebook-W.png" alt="logo" class="footer-social-link"></a>
+                    <a href="/"><img src="assets/images/Logos/Instagram-W.png" alt="logo"
+                            class="footer-social-link"></a>
+                    <a href="/"><img src="assets/images/Logos/Linkdin-W.png" alt="logo" class="footer-social-link"></a>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
 </body>
+
+
 </html>
--- Web design term 3 project/app/views/home/index.php
+++ Web design term 3 project/app/views/home/index.php
@@ -22,6 +22,27 @@ $users = $repository->getUsers();
     <p>No users found.</p>
 <?php endif; ?>
 
+<p>
+To move two children of a Flexbox container to the next row, you have a couple of options depending on the overall layout and design you're aiming for. Flexbox automatically places items in a row or column based on the flex-direction property, but it treats the container as a single row or column. To explicitly move items to a new row within the same Flexbox container, you can use a combination of the flex-wrap property and manipulate the width, flex-basis, or use a pseudo-element as a workaround.
+
+Option 1: Using flex-wrap and adjusting widths
+If you want some items to wrap onto the next line, you can set flex-wrap: wrap; on the container and then control the width of the children so that they naturally wrap to the next line when there's not enough space in the container to fit them in a single line.
+</p>
+<p>
+To move two children of a Flexbox container to the next row, you have a couple of options depending on the overall layout and design you're aiming for. Flexbox automatically places items in a row or column based on the flex-direction property, but it treats the container as a single row or column. To explicitly move items to a new row within the same Flexbox container, you can use a combination of the flex-wrap property and manipulate the width, flex-basis, or use a pseudo-element as a workaround.
+
+Option 1: Using flex-wrap and adjusting widths
+If you want some items to wrap onto the next line, you can set flex-wrap: wrap; on the container and then control the width of the children so that they naturally wrap to the next line when there's not enough space in the container to fit them in a single line.
+</p>
+
+<p>
+To move two children of a Flexbox container to the next row, you have a couple of options depending on the overall layout and design you're aiming for. Flexbox automatically places items in a row or column based on the flex-direction property, but it treats the container as a single row or column. To explicitly move items to a new row within the same Flexbox container, you can use a combination of the flex-wrap property and manipulate the width, flex-basis, or use a pseudo-element as a workaround.
+
+Option 1: Using flex-wrap and adjusting widths
+If you want some items to wrap onto the next line, you can set flex-wrap: wrap; on the container and then control the width of the children so that they naturally wrap to the next line when there's not enough space in the container to fit them in a single line.
+</p>
+
+
 <?php
 include __DIR__ . '/../footer.php';
 ?>
