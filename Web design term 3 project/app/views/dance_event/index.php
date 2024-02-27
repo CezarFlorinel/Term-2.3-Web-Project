32a7934c97ba8afab1bee8ec1f5abc83aa7e322e
biancab20 <705366@student.inholland.nl>
2024-02-27 12:13:12 +0100

    started on dance event

--- Web design term 3 project/app/views/dance_event/index.php
+++ Web design term 3 project/app/views/dance_event/index.php
@@ -1,1 +1,35 @@
-this is the dance event
+<?php
+include __DIR__ . '/../header.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>DANCE</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="CSS_files/dance_event.css">
+</head>
+
+<body>
+
+<div class="container-fluid">
+    <img src="assets\images\dance_event\artists\homepage_dance\image 5.png" class="img-fluid" alt="Dance event">
+    this is the dance event
+</div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
+
+
+
+<?php
+include __DIR__ . '/../footer.php';
+?>
