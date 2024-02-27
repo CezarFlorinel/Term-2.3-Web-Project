3cd7a30cacc31fc4b9ded47351da8fb1463a48a4
biancab20 <705366@student.inholland.nl>
2024-02-27 23:27:12 +0100

    update on dance but not finished

--- Web design term 3 project/app/views/dance_event/index.php
+++ Web design term 3 project/app/views/dance_event/index.php
@@ -1,8 +1,3 @@
-<?php
-include __DIR__ . '/../header.php';
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,26 +5,98 @@ include __DIR__ . '/../header.php';
     <title>DANCE</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="CSS_files/dance_event.css">
 </head>
 
 <body>
+    <?php
+    include __DIR__ . '/../header.php';
+    ?>
+    <!-- you can change this container with a card that has a image as a background -->
+    <!-- <div class="container-fluid" id="start-image-section">
+        <div class="start-image-text">
+            <h1 class="text-1-h">HAARLEM</h1>
+            <h1 class="text-2-h">Festival</h1>
+            <h1 class="text-3-h">DANCE!</h1>
+        </div> -->
 
-<div class="container-fluid">
-    <img src="assets\images\dance_event\artists\homepage_dance\image 5.png" class="img-fluid" alt="Dance event">
-    this is the dance event
-</div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </div>
 
+    <div class="card text-bg-dark">
+        <img src="assets\images\dance_event\artists\homepage_dance\image 5.png" class="card-img"
+            alt="Dance event picture">
+        <div class="card-img-overlay">
+            <h1 class="text-1-h">HAARLEM</h1>
+            <h1 class="text-2-h">Festival</h1>
+            <h1 class="text-3-h">DANCE!</h1>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="card-group">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                </div>
+                <img src="..." class="card-img-top" alt="...">
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                </div>
+                <img src="..." class="card-img-top" alt="...">
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                </div>
+                <img src="..." class="card-img-top" alt="...">
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                </div>
+                <img src="..." class="card-img-top" alt="...">
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                </div>
+                <img src="..." class="card-img-top" alt="...">
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                </div>
+                <img src="..." class="card-img-top" alt="...">
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+    <?php
+    include __DIR__ . '/../footer.php';
+    ?>
 </body>
 
 </html>
-
-
-
-<?php
-include __DIR__ . '/../footer.php';
-?>
