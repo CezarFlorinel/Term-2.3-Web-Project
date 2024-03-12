bc67244e8394f0160aa2729ae106d6e11f569551
CezarFlorinel <706431@student.inholland.nl>
2024-03-12 13:03:18 +0100

    Pdf generator lobrayr

--- Web design term 3 project/app/vendor/composer/autoload_real.php
+++ Web design term 3 project/app/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitfdbf34b9944a7abd3eceb56cc305c687
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitfdbf34b9944a7abd3eceb56cc305c687', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitfdbf34b9944a7abd3eceb56cc305c687', 'loadClassLoader'));
