77f36d6de1f49412012c88c0aa715942806e199c
CezarFlorinel <706431@student.inholland.nl>
2024-03-28 16:51:45 +0100

    model classes for payment

---
+++ Web design term 3 project/app/models/tickets/danceTicket.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Models\Order_And_Invoice;
+
+class DanceTicket implements \JsonSerializable
+{
+    public int $id;
+    public string $dateTime;
+    public string $location;
+    public float $price;
+    public string $singer;
+    public int $totalQuantityOfAvailableTickets;
+    public string $sessionType;
+
+    public function __construct(int $id, string $dateTime, string $location, float $price, string $singer, int $totalQuantityOfAvailableTickets, string $sessionType)
+    {
+        $this->id = $id;
+        $this->dateTime = $dateTime;
+        $this->location = $location;
+        $this->price = $price;
+        $this->singer = $singer;
+        $this->totalQuantityOfAvailableTickets = $totalQuantityOfAvailableTickets;
+        $this->sessionType = $sessionType;
+    }
+
+
+    public function jsonSerialize(): mixed
+    {
+        return get_object_vars($this);
+    }
+}
