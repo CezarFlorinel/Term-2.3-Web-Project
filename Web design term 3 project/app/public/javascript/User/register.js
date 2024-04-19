a38bb94edf6d9eecace055427399735dbc1a6406
CezarFlorinel <706431@student.inholland.nl>
2024-04-19 22:11:11 +0200

    sad

--- Web design term 3 project/app/public/javascript/User/register.js
+++ Web design term 3 project/app/public/javascript/User/register.js
@@ -1,50 +1,51 @@
 //Registration form
 
 document.addEventListener("DOMContentLoaded", function () {
-    const registrationForm = document.getElementById("registrationForm");
-    console.log("this is working");
-    if (registrationForm) {
-      registrationForm.addEventListener("registerButton", function (event) {
-        event.preventDefault();
-  
-        const name = document.getElementById("name").value;
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-        const passwordConfirm = document.getElementById("confirmPassword").value;
-  
-        if (!name || !email || !password || !passwordConfirm) {
-          alert("Please fill in all fields.");
-          return;
-        }
-  
-        if (password !== passwordConfirm) {
-          alert("Passwords do not match.");
-          return;
-        }
-  
-        const formData = {
-          name: name,
-          email: email,
-          password: password,
-          passwordConfirm: passwordConfirm,
-          userRole: "Member",
-        };
-        console.log(formData);
-  
-        fetch("http://localhost/api/user", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
+  const registrationForm = document.getElementById("registrationForm");
+  console.log("this is working");
+
+  if (registrationForm) {
+    registrationForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+      console.log("this is working --2")
+      const name = document.getElementById("name").value;
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      const passwordConfirm = document.getElementById("confirmPassword").value;
+
+      if (!name || !email || !password || !passwordConfirm) {
+        alert("Please fill in all fields.");
+        return;
+      }
+
+      if (password !== passwordConfirm) {
+        alert("Passwords do not match.");
+        return;
+      }
+
+      const formData = {
+        name: name,
+        email: email,
+        password: password,
+        passwordConfirm: passwordConfirm,
+        userRole: "Member",
+      };
+      console.log(formData);
+
+      fetch('api/user/create', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          //window.location.href = "/login/index";
         })
-          .then((response) => response.json())
-          .then((data) => {
-            //window.location.href = "/login/index";
-          })
-          .catch((error) => {
-            console.error("Error:", error);
-          });
-      });
-    }
-  });
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
+  }
+});
