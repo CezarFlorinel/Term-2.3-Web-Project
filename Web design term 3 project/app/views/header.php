bbe48d58eee1a3e3fe20bde568cdd863369d15ed
Wouter Altepost <wout.altepost@gmail.com>
2024-04-24 18:06:01 +0200

    header + scalability fixed

--- Web design term 3 project/app/views/header.php
+++ Web design term 3 project/app/views/header.php
@@ -5,8 +5,7 @@
     <title>Festival Project</title>
     <link rel="icon" type="image/png" href="assets/images/logos/H.png">
     <link rel="stylesheet" href="CSS_files/header.css">
-    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:500|Zen+Antique|Allerta+Stencil&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:500|Zen+Antique|Allerta+Stencil&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -15,26 +14,30 @@
             <a href="/" class="logoLink">
                 <img class="logo" src="assets/images/Logos/Logo-Festival.png" alt="Logo">
             </a>
+            <input type="checkbox" id="menu-toggle" class="menu-toggle">
+            <label for="menu-toggle" class="menu-icon" id="menu-icon">
+                <img src="assets/images/elements/hamburger_Icon.jpg" alt="Menu">
+            </label>
             <nav class="navigation">
-                <a href="/mainpageadmin" class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/mainpageadmin') echo 'active'; ?>">Admin</a>
                 <a href="/" class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/') echo 'active'; ?>">Home</a>
                 <a href="/yummyevent" class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/yummyevent') echo 'active'; ?>">Yummy</a>
-                <a href="/danceevent" class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/danceevent') echo 'active'; ?>">Dance!</a>
-                <a href="/historyevent" class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/historyevent') echo 'active'; ?>">History Tour</a>
+                <a href="/danceevent" class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/danceevent') echo 'active'; ?>">Dance</a>
+                <a href="/historyevent" class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/historyevent') echo 'active'; ?>">Stroll Through History</a>
+                <a href="/mainpageadmin" class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/mainpageadmin') echo 'active'; ?>">Admin</a>
+            </nav>
+            <div class="icons">
                 <a href="/payment">
-                    <img class="icon0" src="assets/images/elements/Shopping cart.png" alt="Logo">
+                    <img class="icon" src="assets/images/elements/Shopping cart.png" alt="Shopping Cart">
                 </a>
                 <a href="/">
-                    <img class="icon1" src="assets/images/elements/Wishlist.png" alt="Logo">
+                    <img class="icon" src="assets/images/elements/Wishlist.png" alt="Wishlist">
                 </a>
                 <a href="/login">
-                    <img class="icon2" src="assets/images/elements/login.png" alt="Logo">
+                    <img class="icon" src="assets/images/elements/login.png" alt="Login">
                 </a>
-                <a href="/">
-                    <img class="language-switcher" src="assets/images/elements/UK-Flag.png" alt="Logo">
-                </a>
-            </nav>
+            </div>
         </header>
+
     </main>
 </body>
 
