21d739c09f779a995e6010af58f1d131e4194e01
biancab20 <705366@student.inholland.nl>
2024-04-28 21:52:09 +0200

    edit on user personal information.html

--- Web design term 3 project/app/public/javascript/User/userPersonalInformation.js
+++ Web design term 3 project/app/public/javascript/User/userPersonalInformation.js
@@ -1,3 +1,9 @@
 import { handleApiResponse, checkText } from '../Utilities/handle_data_checks.js';
 import ErrorHandler from '../Utilities/error_handler_class.js';
 const errorHandler = new ErrorHandler();
+
+//Getting the user personal information and creating the table
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+})
