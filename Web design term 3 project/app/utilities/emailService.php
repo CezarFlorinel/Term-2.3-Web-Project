cd53dcaf002c3a30b65a49817bbc270229fd8c11
biancab20 <705366@student.inholland.nl>
2024-05-14 12:05:03 +0200

    trying search functionality...not working yet

--- Web design term 3 project/app/utilities/emailService.php
+++ Web design term 3 project/app/utilities/emailService.php
@@ -11,19 +11,37 @@ use PHPMailer\PHPMailer\Exception;
 class EmailService
 {
     private $userService;
-    private static function configureMailer() {
+    private static function configureMailer()
+    {
 
         $mail = new PHPMailer(true);
         require '../config/emailconfig.php';
 
         $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com';
-            $mail->SMTPAuth = true;
-            $mail->Username = $emailAddress;
-            $mail->Password = $key;
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            $mail->Port = 587;
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = $emailAddress;
+        $mail->Password = $key;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
 
         return $mail;
+    }
+    public static function sendEmail($email, $subject, $body)
+    {
+        try {
+            $mail = self::configureMailer(); // Configure PHPMailer
+            // Set email content
+            $mail->addAddress($email);
+            $mail->Subject = $subject;
+            $mail->Body = $body;
+            $mail->isHTML(true);
+
+            // Send email
+            $mail->send();
+        } catch (Exception $e) {
+            // Handle error
+            // You can log the error or throw a custom exception
+        }
     }
 }
