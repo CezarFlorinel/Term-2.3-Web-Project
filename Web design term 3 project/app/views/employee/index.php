98012915119743a8590e0872ad03df163863ac59
Wouter Altepost <wout.altepost@gmail.com>
2024-05-20 12:29:39 +0200

    front end qr scanner

--- Web design term 3 project/app/views/employee/index.php
+++ Web design term 3 project/app/views/employee/index.php
@@ -8,40 +8,38 @@ include __DIR__ . '/../header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
 <body>
-    <div class="container mx-auto">
-        <div class="flex justify-center">
+    <section class="flex flex-col items-center justify-center min-h-8 mt-12">
+        <h1 class="text-center text-3xl font-bold mb-2">Scan your QR code below!</h1>
+        <div class="container mx-auto flex flex-col items-center justify-center">
             <div class="w-full md:w-1/2">
                 <div class="card border border-gray-200 shadow-lg rounded-lg">
-                    <div class="card-body p-6">
-                        <h1 class="card-title text-center text-2xl font-bold mb-4">QR Code Scanner</h1>
-                        <div class="text-center">
-                            <video id="video" class="w-72 h-48 border border-gray-300"></video>
+                    <div class="card-body p-8">
+                        <div class="text-center mb-4">
+                            <video id="video" class="w-80 h-52 border border-gray-300"></video>
                         </div>
-                        <div class="text-center mt-3">
-                            <button id="startButton" class="btn bg-blue-500 text-white py-2 px-4 rounded">Start
-                                Scanning</button>
+                        <div class="text-center mt-4">
+                            <button id="startButton" class="bg-blue-300 text-white py-4 px-8 rounded-lg text-lg">Start Scanning</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="container mx-auto mt-6">
-        <div class="flex justify-center">
+        <div class="container mx-auto mt-4 flex justify-center">
             <div class="w-full md:w-1/2">
                 <div class="card border border-gray-200 shadow-lg rounded-lg">
-                    <div class="card-body p-6" id="ticketInfoCard">
+                    <div class="card-body p-8" id="ticketInfoCard">
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <script type="module" src="/../javascript/Employee/employee_view.js"></script>
-    <script src="https://unpkg.com/@zxing/library@latest"></script>
+        <script type="module" src="/../javascript/Employee/employee_view.js"></script>
+        <script src="https://unpkg.com/@zxing/library@latest"></script>
+    </section>
 </body>
 
 </html>
