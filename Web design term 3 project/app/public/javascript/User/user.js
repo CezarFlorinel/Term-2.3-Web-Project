8b7a468d56db39293424691ae7b05c538376dadc
biancab20 <705366@student.inholland.nl>
2024-06-23 14:37:27 +0200

    fixed search function

--- Web design term 3 project/app/public/javascript/User/user.js
+++ Web design term 3 project/app/public/javascript/User/user.js
@@ -11,10 +11,10 @@ function searchFunction() {
   tr = tbody.getElementsByTagName("tr");
 
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[3]; // Adjusted to column index for Name
+    td = tr[i].getElementsByTagName("td")[2]; 
     if (td) {
       txtValue = td.textContent || td.innerText;
-      if (txtValue.toLowerCase().indexOf(filter) > -1) {
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
